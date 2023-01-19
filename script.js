28bc10b84bab2dc1f9d7528b309fe81abdf216ba
Janetta Kataoeva <jkattoeva@gmail.com>
2023-01-20 04:47:22 +0600

    added for loop with timeout method, thatsshowusdif

---
+++ script.js
@@ -0,0 +1,15 @@
+// What the difrence between let & var & const
+// const cannot changed , thats better to use for arrays and objects
+
+// let is even more like var , modern web developers usually use let when a vatiable value should allow updates
+
+for (var x = 0; x < 3; x++) {
+  setTimeout(() => {
+    console.log(x);
+  }, 1000);
+}
+for (let y = 0; y < 3; y++) {
+  setTimeout(() => {
+    console.log(y);
+  }, 2000);
+}
