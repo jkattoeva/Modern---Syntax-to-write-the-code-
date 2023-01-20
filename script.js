6e2dd9e3c5619c722f1b00fe6b425e158db1f176
Janetta Kataoeva <jkattoeva@gmail.com>
2023-01-20 14:47:43 +0600

    add example with total and adding comments

--- script.js
+++ script.js
@@ -3,13 +3,46 @@
 
 // let is even more like var , modern web developers usually use let when a vatiable value should allow updates
 
-for (var x = 0; x < 3; x++) {
-  setTimeout(() => {
-    console.log(x);
-  }, 1000); // 3 3
+// for (var x = 0; x < 3; x++) {
+//   setTimeout(() => {
+//     console.log(x);
+//   }, 1000); // 3 3
+// }
+// for (let y = 0; y < 3; y++) {
+//   setTimeout(() => {
+//     console.log(y);
+//   }, 2000); // 0 1 2
+// }
+
+// Spread operator ...
+
+// var arr = [1, 2, 3, 4, 5];
+// var arr2 = [6, 7, 8, 9, 10];
+
+// var bothArr = [...arr, ...arr2];
+
+// var obj = {
+//   name: "Janetta",
+//   role: "Mentor",
+// };
+// var obj2 = {
+//   location: "Kyrgyzstan",
+//   students: ["jk", "kg", "etc"],
+// };
+// var bothObj = {
+//   ...obj,
+//   ...obj2,
+// };
+// console.log(bothArr, bothObj);
+
+// function with spread operator
+// total add x / y
+function addAll(...numbers) {
+  let total = 0;
+  for (let num of numbers) {
+    total += num;
+    console.log(num, total);
+  }
+  return total;
 }
-for (let y = 0; y < 3; y++) {
-  setTimeout(() => {
-    console.log(y);
-  }, 2000); // 0 1 2
-}
+addAll(6, 5, 4);
